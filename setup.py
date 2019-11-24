727ff37efcb46844c40104a0c3a582bfe3f984ca
iamHarin17 <harinramesh17@gmail.com>
2019-11-24 20:39:51 +0530

    Added install_requires

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
 setup(     
-    name="core",
-    version="0.1",
-    description="Light weight web framework",
-    author='Harin',
-    packages=['core'],
-    install_requires=[]
-    )
+	name="core",
+	version="0.1",
+	packages=['core'],
+	install_requires=['jinja2'],
+
+	description="Light weight web framework",
+	author='Harin Ramesh',
+	project_urls={
+        "Source Code": "https://github.com/iamHarin17/core",
+    },
+)
