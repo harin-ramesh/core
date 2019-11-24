b4b0da3b97c460d40f26d0ea00fbc1b351ff6c7d
iamHarin17 <harinramesh17@gmail.com>
2019-11-24 20:39:15 +0530

    converted f string to format

--- core/template.py
+++ core/template.py
@@ -4,11 +4,10 @@ from jinja2 import Environment, FileSystemLoader
 from core import Response
 
 def render(file_name, context):
-    print(os.environ['path'])
-    path = f"{os.environ['path']}/templates"
-    file_loader = FileSystemLoader(path)
-    env = Environment(loader=file_loader)
-    template = env.get_template(file_name)
-    output = template.render(context)
-    return Response(output)
+	path = "{}/templates".format(os.environ['path'])
+	file_loader = FileSystemLoader(path)
+	env = Environment(loader=file_loader)
+	template = env.get_template(file_name)
+	output = template.render(context)
+	return Response(output)
 
