677e6f7f2ffbb7002a7d0326df3cd0c0b2fae438
iamHarin17 <harinramesh17@gmail.com>
2020-01-04 20:24:09 +0530

    Added templte to project template

--- core/conf/projct_template/view.py
+++ core/conf/projct_template/view.py
@@ -2,9 +2,9 @@ from core import Response
 from core.router import Router
 from core.template import render
 
-def hello(request, name):
-	return render('index.html', {'titile':'Core','msg':'Hello'})
+def hello(request, name=None):
+	return render('index.html', {})
 
 routes = Router()
-routes.add_route(r'/hello/(.*)/$', hello)
+routes.add_route(r'/$', hello)
 
